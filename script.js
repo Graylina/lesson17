48d4860c9b589c84021bfc172fbcb62173f3d8c8
Graylina <uskanli21@gmail.com>
2019-07-09 18:46:23 +0300

    add ready homework

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict";
+// 1
+var sec = 60;
+var hour = 60;
+var hours = 3;
+console.log(sec * hour * hours);
+
+// 2
+var num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num += 1;
+num -= 1;
+alert(num);
+
+// 3
+var a = 10; 
+var b = 2;
+var c = 5;
+console.log(a + b + c);
+
+// 4
+var a = 17;
+var b = 10;
+var c = b - a;
+var d = 7;
+var result = c + d;
+console.log(result);
+
+// 5
+var name = 'Алина';
+console.log('Привет,' + ' ' + name + '!');
